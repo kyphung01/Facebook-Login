30d8674918dc5e56d19d215bd648f8eace983442
kyphung01 <thaipm56@gmail.com>
2016-04-08 10:44:10 +0700

    Facebook Login

---
+++ app/src/main/java/com/example/android/test_stuff/MyApplication.java
@@ -0,0 +1,40 @@
+package com.example.android.test_stuff;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("Vao :", "MyAppAct");
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.android.test_stuff",  // replace with your unique package name
+                    PackageManager.GET_SIGNATURES);
+
+
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
